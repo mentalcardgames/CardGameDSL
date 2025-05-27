397f38b4a7ff83a620380aaeee46dc3a99225b70
Till Hoffmann <tillhoff42@gmail.com>
2025-05-27 12:06:10 +0200

    Working on Documentation

--- src/tests.rs
+++ src/tests.rs
@@ -80,7 +80,7 @@ mod tests {
     fn test_precedence_macro() {
         let mut cgm = CardGameModel::new("precedence_test");
 
-        precedence!("rank", ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"))(&mut cgm.gamedata);
+        precedence!("rank", ("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"))(&mut cgm.gamedata);
         precedence!("suite", ("Clubs", "Diamonds", "Hearts", "Spades"))(&mut cgm.gamedata);
 
         // Test rank precedence
